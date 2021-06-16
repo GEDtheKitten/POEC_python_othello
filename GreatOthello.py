1f3bdf74ba0e16b9a99759066785c5e40e42456a
gg <gedthekitten@gmail.com>
2021-06-16 09:52:10 +0200

    Reimplementation de rechercher et retourner poins - nettoyer

--- GreatOthello.py
+++ GreatOthello.py
@@ -34,53 +34,6 @@ class Plateau:
     def get_value(self, i, j):
         return self.plateau[i][j]
 
-    def retourner_pions_old(self, xInitial, yInitial, xc, yc, color):
-        for i in range(xInitial, xc):
-            for j in range(yInitial, yc):
-                self.plateau[i][j] = color
-                i -= 1
-                j -= 1
-
-    # recherche le pion de couleur + retourner les pions entre le pion posé et le pion de même couleur le plus proche
-    def rechercher_pion_old(self, xc, yc, color):
-        tabSens = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-
-        xInitial = xc
-        yInitial = yc
-
-        # recherche le pion de couleur 'color' le plus proche dans chaque direction
-        for i, j in tabSens:
-            xc += i
-            yc += j
-            while 0 <= xc <= 7 and 0 <= yc <= 7:
-                if self.plateau[xc][yc] == color:
-                    # on a repéré le pion [xc,yc] le plus proche dans cette direction
-                    # on transforme les points entre le point [xInitial, yInitial] et le point repéré [xc, yc]
-                    self.retourner_pions(xInitial, yInitial, xc, yc, color)
-                    xc += i
-                    yc += j
-                # vérifie la présence d'une case vide
-                elif self.plateau[xc][yc] == 0:
-                    break
-                else:
-                    break
-
-
-    # def retourner_pions(self, xInitial, yInitial, xc, yc, sence_x, sence_y, color):
-    #     print('retourner_pions', xInitial, yInitial, xc, yc, sence_x, sence_y, color)
-    #     for i in range(xInitial, xc):
-    #         for j in range(yInitial, yc):
-    #             print('retrouner pions')
-    #             print('i  ', i)
-    #             print('j  ', j)
-    #             print('sence_x  ', sence_x)
-    #             print('sence_y  ', sence_y)
-    #             print('color  ', color)
-    #             self.plateau[i][j] = color
-    #             i -= sence_x
-    #             j -= sence_y
-
-
     def est_sur_le_plateau(self, x_arrive, y_arrive):
         return 0 <= x_arrive <= self.taille_plateau()-1 and 0 <= y_arrive <= self.taille_plateau()-1
 
@@ -98,96 +51,19 @@ class Plateau:
 #           | 0 0 0 0 0 0 0 0
 #           | 0 0 0 0 0 0 0 0
 
-
     def rechercher_pion(self, x_depart, y_depart, color):
         tabSens = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
         for dir_x, dir_y in tabSens:
             x_arrive = x_depart
             y_arrive = y_depart
             nb_pions_a_retourner = []
-            # print()
-            # print('direction du parcour', dir_x, dir_y)
-            # while 1 <= x_arrive <= 6 and 1 <= y_arrive <= 6 and self.plateau[x_arrive][y_arrive] != 0:
-            # x_arrive += dir_x
-            # y_arrive += dir_y
             while self.est_sur_le_plateau(x_arrive, y_arrive) and self.plateau[x_arrive][y_arrive] != 0:
-                # x_arrive += dir_x
-                # y_arrive += dir_y
                 nb_pions_a_retourner.append([x_arrive, y_arrive])
-                # print('x_arrive, y_arrive', x_arrive, y_arrive)
                 if self.plateau[x_arrive][y_arrive] == color:
-                    # print('poins a retourner', nb_pions_a_retourner)
                     for x_pion, y_pion in nb_pions_a_retourner:
                         self.plateau[x_pion][y_pion] = color
                 x_arrive += dir_x
                 y_arrive += dir_y
-
-                    # for i in range(x_arrive, x_depart):
-                    #     for j in range(y_arrive, y_depart):
-                    #         self.plateau[i][j] = color
-
-                            # x_arrive -= dir_x
-                            # y_arrive -= dir_y
-                            # break
-
-
-                    # if x_arrive != x_depart and y_arrive != y_depart:
-                    #     while x_arrive != x_depart and y_arrive != y_depart:
-                    #         self.plateau[x_arrive][y_arrive] == color
-                    #         x_arrive -= dir_x
-                    #         y_arrive -= dir_y
-
-
-
-    # def rechercher_pion_bis(self, xc, yc, color):
-    #     tabSens = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-
-    #     xInitial = xc
-    #     yInitial = yc
-    #     xc_dir = xc
-    #     yc_dir = yc
-    #     print('changement de direction')
-    #     # recherche le pion de couleur 'color' le plus proche dans chaque direction
-    #     for i, j in tabSens:
-    #         xc_dir += i
-    #         yc_dir += j
-    #         print()
-    #         print('changement de direction')
-    #         print('i  ', i)
-    #         print('j  ', j) 
-    #         print('xInitial  ', xInitial)
-    #         print('yInitial  ', yInitial)
-    #         print('xc  ', xc_dir)
-    #         print('yc  ', yc_dir)
-    #         pions_a_retourner = []
-    #         while 0 <= xc_dir <= 7 and 0 <= yc_dir <= 7:
-    #             pions_a_retourner.append([xc_dir, yc_dir])
-    #             while self.plateau[xc_dir][yc_dir] != (color and 0):
-    #                 print('color  ', color)
-    #                 # on a repéré le pion [xc,yc] le plus proche dans cette direction
-    #                 # on transforme les points entre le point [xInitial, yInitial] et le point repéré [xc, yc]
-    #                 print('pions_a_retourner  ', pions_a_retourner)
-    #                 self.retourner_pions(xInitial, yInitial, xc_dir, yc_dir, i, j, color)
-    #                 # print('avance dans la direction')
-    #                 # print('xc  ', xc)
-    #                 # print('yc  ', yc)
-    #                 # print('i  ', i)
-    #                 # print('j  ', j)                   
-    #                 # print('xc trouve  ', xc)
-    #                 # print('yc trouve  ', yc)
-    #                 xc_dir += i
-    #                 yc_dir += j
-    #             # vérifie la présence d'une case vide
-    #             if self.plateau[xc_dir][yc_dir] == 0:
-    #                 break
-    #             else:
-    #                 break
-        '''
-        print(self.plateau[xInitial][yInitial])
-        print(xInitial)
-        print(yInitial)
-        print(self.plateau[4][4])
-        '''
 
     # vérifie la présence d'un autre pion à côté du pion placé
     def verifier_si_saisie_valide(self, xc,yc):
